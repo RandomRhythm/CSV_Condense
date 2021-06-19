c667bc694f63344cc7fa57a0f7d83875be16aaae
Ryan B <randomrhythm@rhythmengineering.com>
2021-06-19 14:36:14 -0600

    Support folder input. Bug fixes and code comments
    Signed-off-by: Ryan B <randomrhythm@rhythmengineering.com>

--- csv_condense.py
+++ csv_condense.py
@@ -6,7 +6,7 @@ import io
 from collections import defaultdict
 
 
-strinputFile = "D:\\logs\\fileforanalysis.csv" #Input file to process
+strinputFile = "D:\\logs\\fileforanalysis.csv" #Input file or folder to process
 strOutPath = "D:\\analysis\\output.csv" #output file
 boolTruncateAtChar = True #truncate key at first space
 strTruncateChar = "\t"
@@ -15,6 +15,8 @@ boolQuoteOutput = True #add quotes around each field
 inputEncoding = "utf-8"
 outputEncoding = "utf-8"
 intNumericAddColumn = 9; #Column containing numeric value to add up. Set to -1 to disable.
+boolHasHeader = True #if no header in CSV then set to false and provide integer values for dictHeader
+boolIgnoreCountErrror = True #skip counting entries where intNumericAddColumn is not numeric
 maxInt = 100000000 #csv.field_size_limit
 dictHeader = dict() #add the column header for each column that will be tracked
 dictNumeric = dict()
@@ -53,6 +55,8 @@ dictNumeric = dict()
 
 #dictHeader["Computer"] = "";
 #dictHeader["Sensor ID"] = "";
+
+
 def writeCSV(fHandle, rowOut):
   if boolQuoteOutput == True:
     fHandle.write("\"" + rowOut.replace("|", "\",\"") + "\"\n")
@@ -60,54 +64,81 @@ def writeCSV(fHandle, rowOut):
     fHandle.write(rowOut.replace("|", ",") + "\n")
 
 dictOutput = dict()
-intRowCount = 0;
-csv.field_size_limit(maxInt)
-with open(strinputFile, "rt", encoding=inputEncoding) as csvfile: #, encoding="utf-16"
-    reader = csv.reader(csvfile, delimiter=',', quotechar='\"')
-    keyitem = "";
-    for row in reader:
-        if intRowCount == 0:
-            
-            for headItem in row:
-                if headItem in dictHeader:
-                    dictHeader[headItem] = intRowCount;
-                intRowCount += 1;
-        elif len(row) > 0: #not blank row
-            keyitem = ""
-            for columnloc in dictHeader:
-               if dictHeader[columnloc] != '': #if found in header
-                 if keyitem == "":
-                    if len(row) >= dictHeader[columnloc]:
-                       keyitem = row[dictHeader[columnloc]];
-                       if boolTruncateAtChar == True:
-                        if keyitem.find(strTruncateChar) > -1:
-                          keyitem = keyitem[:keyitem.find(strTruncateChar)]
-                    else:
-                       f2 = open(strinputFile + ".error", 'a+', encoding="utf-16")
-                       f2.write("".join(row) + "\n") #write header row
-                 else:
-                    keyitem = keyitem + "|" + row[dictHeader[columnloc]];
-            if intNumericAddColumn > -1 and intNumericAddColumn <= len(dictHeader):
-              if row[intNumericAddColumn].isnumeric():
-                intNumeric = int(row[intNumericAddColumn])
-                if keyitem in dictNumeric:
-                  dictNumeric[keyitem] = int(dictNumeric[keyitem]) + int(intNumeric)
-                else:
-                  dictNumeric[keyitem] = int(intNumeric)
-            elif intNumericAddColumn > len(dictHeader):
-              print("Warning: intNumericAddColumn is greather than the CSV column count. Disabling numeric add function")
-              intNumericAddColumn = -1
-            if keyitem in dictOutput:
-                dictOutput[keyitem] += 1;
-            else:
-                dictOutput[keyitem] = 1;
+listFilePaths = []
+
+if os.path.isdir(strinputPath):  
+  for (dirpath, dirnames, filenames) in os.walk(strinputPath):
+    for file in filenames:
+        scanPath = os.path.join(dirpath, file)
+        print(scanPath + "\n")
+        listFilePaths.append(scanPath)
+elif os.path.isfile(strinputPath):  
+  listFilePaths.append(strinputPath)
+
+for strinputFile in listFilePaths:
+  boolFirstRow = boolHasHeader;
+  csv.field_size_limit(maxInt)
+  intRowCount = 0
+  with open(strinputFile, "rt", encoding=inputEncoding) as csvfile: #, encoding="utf-16"
+      reader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+      keyitem = "";
+      for row in reader:
+          if boolFirstRow == True: #parse header row
+              boolFirstRow= False
+              for headItem in row:
+                  if headItem in dictHeader:
+                      dictHeader[headItem] = intRowCount;
+                  intRowCount += 1;
+          else: #not blank row
+              keyitem = ""
+              for columnloc in sorted(dictHeader, key=dictHeader.get): #sort off value to keep column alignment consistent with source
+                 if dictHeader[columnloc] != '': #if found in header
+                   if keyitem == "":
+                      if len(row) >= dictHeader[columnloc]:
+                         keyitem = row[dictHeader[columnloc]];
+                         if boolTruncateAtChar == True:
+                          if keyitem.find(strTruncateChar) > -1:
+                            keyitem = keyitem[:keyitem.find(strTruncateChar)]
+                      else:
+                         f2 = open(strinputFile + ".error", 'a+', encoding="utf-16")
+                         f2.write("".join(row) + "\n") #write header row
+                   elif len(row) <= dictHeader[columnloc]: #Error! can't grab value
+                    print("Row not as long as header: " + str(len(row)) + "<=" + str(dictHeader[columnloc]) + "\n" + " ".join(row))
+                    print(row)
+                   else: #aggregate key names
+                      keyitem = keyitem + "|" + row[dictHeader[columnloc]];
+              if intNumericAddColumn > -1 and intNumericAddColumn < len(row): #intNumericAddColumn has a value and is less than the row count
+                if row[intNumericAddColumn].isnumeric(): #Column we are counting has a numeric value
+                  intNumeric = int(row[intNumericAddColumn])
+                  if keyitem in dictNumeric: #aggregate value
+                    dictNumeric[keyitem] = int(dictNumeric[keyitem]) + int(intNumeric) #math
+                  else: #new addition to dict
+                    dictNumeric[keyitem] = int(intNumeric)
+                else: #non-numeric value for intNumericAddColumn
+                  print("Error! intNumericAddColumn is Not numeric: " + row[intNumericAddColumn] + "\n" + keyitem + "\n" + str(intNumericAddColumn) + "|" + str(len(row)) + "\n" + " ".join(row))
+              elif intNumericAddColumn > len(row) -1 and boolIgnoreCountErrror == True:
+                print("ingoring row: " + " ".join(row) + "\nfrom file " + strinputFile + "\non line " + str(intRowCount))
+              elif intNumericAddColumn > len(row) -1:
+                print("Error: intNumericAddColumn is greather than the CSV column count. \n" + str(intNumericAddColumn) + ">=" + str(len(row)) + "Disabling numeric add function\n" + strinputFile + "\n" + " ".join(row))
+                intNumericAddColumn = -1
+              elif intNumericAddColumn != -1 and keyitem not in dictNumeric: #should only hit here if something is wrong
+                print("not sure how we got here. Check your input file(s) and config? " + keyitem + "\n" + str(intNumericAddColumn) + "|" + str(len(row)) + "\n" + " ".join(row))
+              if keyitem in dictOutput:
+                  dictOutput[keyitem] += 1;
+              else:
+                  dictOutput[keyitem] = 1;
+          intRowCount +=1
 with io.open(strOutPath, "w", encoding=outputEncoding) as f: #encoding="utf-16"
-  for outputline in dictOutput:
-      if boolIncludeCount == True:
-        outputline = outputline + "|" + str(dictOutput[outputline])
+  for lineEntry in dictOutput:
+      if boolIncludeCount == True: #Count of entries combined
+        outputline = lineEntry + "|" + str(dictOutput[lineEntry])
       else:
-        outputline = outputline 
-      if intNumericAddColumn > -1:
-        strCount = "|" + str(dictNumeric[outputline])
+        outputline = lineEntry 
+      if intNumericAddColumn > -1: #Integer aggregation
+        strCount = "|" + str(dictNumeric[lineEntry])
         outputline = outputline + strCount
       writeCSV(f,outputline)
+
+
+
+
